2c4d0296a3ab901a241df9d644c4219040059a44
Medium Kahuna <jud@mac.com>
2023-10-07 12:37:40 -0400

    added src and test1

---
+++ src/test1/test1.ino
@@ -0,0 +1,14 @@
+//. test 1 of ESP32 Nano  capabilities
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
