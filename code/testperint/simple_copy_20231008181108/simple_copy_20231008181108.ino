6f0423292675bc123b929fbf37b191325932eac9
Medium Kahuna <jud@mac.com>
2023-10-10 11:29:11 -0400

    edit spec

--- code/testperint/simple_copy_20231008181108/simple_copy_20231008181108.ino
+++ code/testperint/simple_copy_20231008181108/simple_copy_20231008181108.ino
@@ -8,16 +8,24 @@
 #include "tinyTimeR.hpp"
 static tinyTimeR tt;
 #define LED_PIN (13)
+#define LED_RED_PIN (14)
+#define LED_GREEN_PIN (15)
+#define LED_BLUE_PIN (16)
 
 //=========================================
 // ledOnOff
 //=========================================
 unsigned int count;
+unsigned int color = 0;
 void ledOnOff(void) {
-  if(++count >= 500)
+  if(++count >= 1000)
   {
-     count -= 500;
+     count -= 1000;
      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+     digitalWrite(LED_RED_PIN, (color & 1));
+     digitalWrite(LED_GREEN_PIN, (color & 2));
+     digitalWrite(LED_BLUE_PIN, (color & 4));
+     --color;
   }
 }
 
@@ -28,7 +36,11 @@ void setup(void) {
   Serial.begin(115200); delay(1000);
 
   pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_RED_PIN, OUTPUT);
   digitalWrite(LED_PIN, 1);
+  digitalWrite(LED_RED_PIN, 1);
+  digitalWrite(LED_GREEN_PIN, 1);
+  digitalWrite(LED_BLUE_PIN, 1);
   count = 0;
   
   tt.begin(ledOnOff, 1000);
